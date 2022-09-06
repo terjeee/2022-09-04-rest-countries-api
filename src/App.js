dc97307ee1308186a97eaef9aac404e1b45eb82d
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-06 04:53:59 +0200

    generelt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import NavBar from "./components/NavBar/NavBar";
+import Countries from "./components/Countries/Countries";
+
 function App() {
-  return;
+  return (
+    <div>
+      <NavBar />
+      <main>
+        <Countries />
+      </main>
+    </div>
+  );
 }
 
 export default App;
