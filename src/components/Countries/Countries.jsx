30a2d9fa2c959abf1090506f5ab3d5beae0f7e22
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-07 18:47:58 +0200

    filter input & region, samlet

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+// import InfiniteScroll from "react-infinite-scroll-component";
 
 import Country from "./components/Country";
 
 import css from "./Countries.module.scss";
 
 function Countries() {
-  const [countries, setCountries] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  const [filterInput, setFilterInput] = useState("");
+  const [filterRegion, setFilterRegion] = useState("");
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -21,32 +26,69 @@ function Countries() {
       });
   }, []);
 
+  const handleFilterInput = (event) => {
+    setFilterInput(event.target.value.toLowerCase());
+  };
+
+  const handleFilterRegion = (event) => {
+    setFilterRegion(event.target.value.toLowerCase());
+  };
+
+  useEffect(() => {
+    const filteredList = countries.filter(
+      (country) =>
+        country.region.toLowerCase().includes(filterRegion) &&
+        country.name.common.toLowerCase().includes(filterInput)
+    );
+
+    setFilteredCountries(filteredList);
+  }, [countries, filterInput, filterRegion]);
+
   return (
     <section className={css.countries}>
       <div className={css.container}>
         <div className={css.filter}>
-          <input className={css.input} type="text" />
-          <select className={css.select} name="filter-select" id="filter-select">
+          <form>
+            <button>:)</button>
+            <input className={css.input} onChange={handleFilterInput} type="text" />
+          </form>
+          <select
+            className={css.select}
+            onChange={handleFilterRegion}
+            name="filter-select"
+            id="filter-select"
+          >
+            <option value="">All Continents</option>
             <option value="Africa">Africa</option>
             <option value="Americas">Americas</option>
             <option value="Asia">Asia</option>
-            <option value="Europa">Europa</option>
+            <option value="Europe">Europe</option>
             <option value="Oceania">Oceania</option>
-            <option value="All">All Continents</option>
           </select>
         </div>
         <div className={css.list}>
-          {countries &&
-            countries.map((country) => (
-              <Country
-                key={country}
-                name={country.name.common}
-                flag={country.flags.png}
-                population={country.population}
-                capital={country.capital}
-                region={country.region}
-              />
-            ))}
+          {/* <InfiniteScroll
+            dataLength={displayCountries.length}
+            next={fetchData}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+              <p style={{ textAlign: "center" }}>
+                <b>The End.</b>
+              </p>
+            }
+          > */}
+          {filteredCountries.map((country, index) => (
+            <Country
+              key={index}
+              name={country.name.common}
+              flag={country.flags.svg}
+              population={country.population}
+              capital={country.capital}
+              region={country.region}
+            />
+          ))}
+          {/* </InfiniteScroll> */}
         </div>
       </div>
     </section>
