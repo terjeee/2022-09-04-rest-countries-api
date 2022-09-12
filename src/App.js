1882e02fe2090c057dfa3528b4d520c9c49741f8
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-12 06:29:21 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import themeContext from "./context/themeContext";
+
 import NavBar from "./components/NavBar/NavBar";
 import Countries from "./routes/Countries/Countries";
 
 function App() {
+  const ctxTheme = useContext(themeContext);
+
+  console.log(ctxTheme.theme);
+
   return (
-    <div>
+    <div id={ctxTheme.theme}>
       <NavBar />
       <main>
         <Countries />
