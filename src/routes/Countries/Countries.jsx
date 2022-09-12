cf858460f19f6378439d8c98b3a266c6429bf60f
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-12 06:29:40 +0200

    styling

--- src/routes/Countries/Countries.jsx
+++ src/routes/Countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 // import InfiniteScroll from "react-infinite-scroll-component";
 
+import { BiSearchAlt2 } from "react-icons/bi";
 import Country from "./components/Country";
 
 import css from "./Countries.module.scss";
@@ -12,6 +13,7 @@ function Countries() {
   const [filterInput, setFilterInput] = useState("");
   const [filterRegion, setFilterRegion] = useState("");
 
+  // fetch countries onMount
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => {
@@ -34,6 +36,7 @@ function Countries() {
     setFilterRegion(event.target.value.toLowerCase());
   };
 
+  // apply filter to filteredCountries
   useEffect(() => {
     const filteredList = countries.filter(
       (country) =>
@@ -48,23 +51,28 @@ function Countries() {
     <section className={css.countries}>
       <div className={css.container}>
         <div className={css.filter}>
-          <form>
-            <button>:)</button>
-            <input className={css.input} onChange={handleFilterInput} type="text" />
+          <form id="theme__el">
+            <button className={css.button} id="theme__el" disabled>
+              <BiSearchAlt2 />
+            </button>
+            <input
+              className={css.input}
+              onChange={handleFilterInput}
+              type="text"
+              placeholder="Search"
+              id="theme__el"
+            />
           </form>
-          <select
-            className={css.select}
-            onChange={handleFilterRegion}
-            name="filter-select"
-            id="filter-select"
-          >
-            <option value="">All Continents</option>
-            <option value="Africa">Africa</option>
-            <option value="Americas">Americas</option>
-            <option value="Asia">Asia</option>
-            <option value="Europe">Europe</option>
-            <option value="Oceania">Oceania</option>
-          </select>
+          <div className={css.div__select} id="theme__el">
+            <select onChange={handleFilterRegion} name="filter-select" id="theme__el">
+              <option value="">All Continents</option>
+              <option value="Africa">Africa</option>
+              <option value="Americas">Americas</option>
+              <option value="Asia">Asia</option>
+              <option value="Europe">Europe</option>
+              <option value="Oceania">Oceania</option>
+            </select>
+          </div>
         </div>
         <div className={css.list}>
           {/* <InfiniteScroll
