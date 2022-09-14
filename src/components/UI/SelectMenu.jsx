7ed39aa0116a347f7e29e69b32d280e1d00f7243
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-14 17:14:35 +0200

    byttet mappe

--- src/components/UI/SelectMenu.jsx
+++ src/components/UI/SelectMenu.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import themeContext from "../../../context/themeContext";
+import themeContext from "../../context/themeContext";
 
 import Select from "react-select";
 
