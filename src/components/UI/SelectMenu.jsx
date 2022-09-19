2f7b0f2565d7980267105f28f7a494a1cfc6e200
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-19 07:02:35 +0200

    styles, @media

--- src/components/UI/SelectMenu.jsx
+++ src/components/UI/SelectMenu.jsx
@@ -2,49 +2,37 @@ import { useContext } from "react";
 import themeContext from "../../context/themeContext";
 
 import Select from "react-select";
-
-const light = {
-  control: (theme) => ({
-    ...theme,
-    width: "auto",
-    padding: "5px 5px",
-    backgroundColor: "hsl(0, 0%, 100%)",
-    fontSize: "15px",
-    border: "none",
-  }),
-  option: (theme) => ({
-    ...theme,
-    backgroundColor: "hsl(0, 0%, 100%)",
-    fontSize: "15px",
-    outlineStyle: "none",
-  }),
-};
-
-const dark = {
-  control: (theme) => ({
-    ...theme,
-    width: "auto",
-    padding: "5px 5px",
-    backgroundColor: "hsl(209, 23%, 22%)",
-    color: "#FFF",
-    fontSize: "15px",
-    border: "none",
-  }),
-  option: (theme) => ({
-    ...theme,
-    backgroundColor: "hsl(209, 23%, 22%)",
-    color: "#FFF",
-    fontSize: "15px",
-    border: "none",
-  }),
-};
+import css from "./SelectMenu.module.scss";
 
 export default function SelectMenu(props) {
   const ctx = useContext(themeContext);
 
+  const styles = {
+    control: (base, state) => ({
+      ...base,
+      padding: "5px 5px",
+      backgroundColor: ctx.theme === "light" ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)",
+      border: "none",
+      boxShadow: "none",
+      cursor: "pointer",
+    }),
+    singleValue: (base) => ({
+      ...base,
+      color: "hsl(0, 0%, 52%)",
+    }),
+    menu: (base) => ({
+      ...base,
+      backgroundColor: ctx.theme === "light" ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)",
+      color: "hsl(0, 0%, 52%)",
+      cursor: "pointer",
+    }),
+    option: (base) => ({ ...base, cursor: "pointer", color: "hsl(0, 0%, 52%)" }),
+  };
+
   return (
     <Select
-      styles={ctx.theme === "light" ? light : dark}
+      className={css.select__container}
+      styles={styles}
       onChange={props.onChange}
       placeholder={props.placeholder}
       isSearchable={props.isSearchable}
